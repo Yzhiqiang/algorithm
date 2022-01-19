98831f4bf0321e234ad8bde71e0e12c3bfe32b56
Yzhiqiang <2595889669@qq.com>
2022-01-19 23:40:01 +0800

    11

---
+++ src/com/yu/day8/Main.java
@@ -0,0 +1,19 @@
+package com.yu.day8;
+
+import java.util.Scanner;
+
+/**
+ * @Author:Yuzhiqiang
+ * @Description:
+ * @Date: Create in 19:13 2022/1/19
+ * @Modified By:
+ */
+public class Main {
+    static int big = 1000000007;
+    static boolean[] flag = new boolean[7];
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.
+    }
+
+}
