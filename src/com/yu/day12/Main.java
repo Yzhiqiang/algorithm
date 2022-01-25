2f2f44dbf89b1a4d9cdf7b82872ced1d87b5b1d7
Yzhiqiang <2595889669@qq.com>
2022-01-25 23:46:27 +0800

    11

---
+++ src/com/yu/day12/Main.java
@@ -0,0 +1,14 @@
+package com.yu.day12;/**
+*@Author:Yuzhiqiang
+*@Description:
+*@Date: Create in 21:52 2022/1/25
+*@Modified By:
+*/
+public class Main {
+    public static void main(String[] args) {
+        String str = "123121";
+        str.length();
+        char[][] ch = new char[10][10];
+        ch[0][0] = str[0];
+    }
+}
