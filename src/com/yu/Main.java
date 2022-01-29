6329577d2c5c0c216b91f4959103bfad4041146e
Yzhiqiang <2595889669@qq.com>
2022-01-29 22:01:42 +0800

    11

--- src/com/yu/Main.java
+++ src/com/yu/Main.java
@@ -1,14 +1,35 @@
 package com.yu;
-
-/**
- * @Author:Yuzhiqiang
- * @Description:
- * @Date: Create in 16:25 2022/1/27
- * @Modified By:
- */
-public class Main {
+import java.io.*;
+import java.util.*;
+class Main {
     public static void main(String[] args) {
-        String str = "30", s = "3";
-        System.out.println(str.compareTo(s));
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put("key1", "value1");
+        hashMap.put("key2", "value2");
+        hashMap.put("key3", "value3");
+        hashMap.put("key4", "value4");
+        hashMap.put("key5", "value5");
+
+        List<Map.Entry<String, String>> set = hashMap.entrySet();
+        for (Map.Entry<String, String> stringStringEntry : set) {
+
+        }
+    }
+}
+class Person implements Comparator<Person> {
+    public int age;
+    public Person() {}
+    public Person(int age) {
+        this.age = age;
+    }
+    public int compare(Person o1, Person o2) {
+        if(o1.age > o2.age) return 1;
+        else if(o1.age < o2.age) return -1;
+        else return 0;
+    }
+
+    @Override
+    public String toString() {
+        return ""+this.age;
     }
 }
