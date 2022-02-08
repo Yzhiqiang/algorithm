5e4196dcfe35e1455f6c57c0138d23933f3814f3
Yzhiqiang <2595889669@qq.com>
2022-02-08 23:23:12 +0800

    11

--- src/com/yu/day13/Main.java
+++ src/com/yu/day13/Main.java
@@ -2,9 +2,9 @@ package com.yu.day13;
 import java.util.*;
 class Main {
     public static void main(String[] args) {
-        String str = "-123";
-        str.sub
-        System.out.println(Integer.valueOf(str));
+        boolean[] flag = new boolean[5];
+        System.out.println(flag[0]);
+
     }
 
 }
---
+++ src/com/yu/day13/test.java
@@ -0,0 +1,10 @@
+package com.yu.day13;
+
+/**
+ * @Author:Yuzhiqiang
+ * @Description:
+ * @Date: Create in 23:22 2022/2/8
+ * @Modified By:
+ */
+public class test {
+}
