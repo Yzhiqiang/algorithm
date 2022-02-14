824d6fb33e12d8fbbb8115829d706ce7fe0b6321
Yzhiqiang <2595889669@qq.com>
2022-02-14 23:40:37 +0800

    11

--- src/com/yu/day14/Main.java
+++ src/com/yu/day14/Main.java
@@ -1,5 +1,7 @@
 package com.yu.day14;
 
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
 /**
  * @Author:Yuzhiqiang
  * @Description:
@@ -7,4 +9,8 @@ package com.yu.day14;
  * @Modified By:
  */
 public class Main {
+    public static void main(String[] args) {
+        String Str = "123";
+        System.out.println(Integer.parseInt(Str));
+    }
 }
